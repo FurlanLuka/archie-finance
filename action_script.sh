d0f474d69940b699459c713e60afaea342d339fc
Luka Furlan <luka@Lukas-MacBook-Pro.local>
2022-03-16 18:15:02 +0100

    Fixed build command in development deployment workflow

--- action_script.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-echo ::set-output name=SOURCE_NAME::${GITHUB_REF#refs/*/}
-echo ::set-output name=SOURCE_BRANCH::${GITHUB_REF#refs/heads/}
-echo ::set-output name=SOURCE_TAG::${GITHUB_REF#refs/tags/}
-export TAG=${GITHUB_REF#refs/tags/}
-export TAG_WITHOUT_TYPE=${TAG#app-}
-echo ::set-output name=APP_NAME::${TAG_WITHOUT_TYPE%-v*}
