c34d1602542e7c1b7872aef0fe0dd454bc448771
Luka Furlan <luka@Lukas-MacBook-Pro.local>
2022-03-16 16:05:14 +0100

    NX initialization

--- apps/archie-auth/jest.config.js
+++ apps/archie-auth/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'archie-web-apps',
+  displayName: 'archie-auth',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/archie-web-apps',
+  coverageDirectory: '../../coverage/apps/archie-auth',
 };
---
+++ apps/archie-auth/src/app/app.tsx
@@ -0,0 +1,11 @@
+import _styles from './app.module.css';
+
+export function App() {
+  return (
+    <>
+      Hello
+    </>
+  );
+}
+
+export default App;
