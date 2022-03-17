85b20af984fd1b94b27a78f6b50fe3771baa0ee9
Luka Furlan <luka@Lukas-MacBook-Pro.local>
2022-03-17 01:13:04 +0100

    Added simple mfa-router

--- apps/archie-auth/src/constants.ts
+++ apps/archie-auth/src/constants.ts
@@ -1,2 +1,2 @@
-// export const API_URL: string = 'https://api.dev.archie.finance'
-export const API_URL: string = 'http://localhost'
+export const API_URL: string = 'https://api.dev.archie.finance'
+// export const API_URL: string = 'http://localhost'
