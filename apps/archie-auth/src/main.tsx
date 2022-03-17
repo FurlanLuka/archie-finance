d5697b52430966d4e234f26c5b5ec6be0403005a
Luka Furlan <luka@Lukas-MacBook-Pro.local>
2022-03-17 01:30:51 +0100

    Added react query for fetching

--- apps/archie-auth/src/main.tsx
+++ apps/archie-auth/src/main.tsx
@@ -2,12 +2,17 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { ErrorBoundary } from './components/error-boundary/error-boundary';
 import { MfaRouter } from './mfa-router/mfa-router';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <StrictMode>
-    <ErrorBoundary>
-      <MfaRouter />
-    </ErrorBoundary>
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary>
+        <MfaRouter />
+      </ErrorBoundary>
+    </QueryClientProvider>
   </StrictMode>,
   document.getElementById('root'),
 );
--- apps/archie-auth/src/mfa-router/mfa-router.tsx
+++ apps/archie-auth/src/mfa-router/mfa-router.tsx
@@ -1,35 +1,40 @@
 import { BallTriangle } from 'react-loading-icons';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import {
   extractQueryParameters,
   getMfaRecord,
   MfaRecord,
 } from './mfa-router-helpers';
-import useAsyncEffect from 'use-async-effect';
 import { MfaSetup } from './routes/mfa-setup/mfa-setup';
 import { Mfa } from './routes/mfa/mfa';
+import { useQuery } from 'react-query';
 
 export const MfaRouter: React.FC = () => {
-  const [mfaRecord, setMfaRecord] = useState<MfaRecord | undefined>();
+  const queryParameters = useMemo(() => extractQueryParameters(), []);
 
-  useAsyncEffect(async () => {
-    const queryParameters = extractQueryParameters();
-
-    const mfaRecord = await getMfaRecord(queryParameters.sessionToken);
-
-    setMfaRecord(mfaRecord);
-  }, []);
-
-  if (mfaRecord) {
-    if (mfaRecord.totpSecret) {
-      return <MfaSetup secret={mfaRecord.totpSecret} />;
-    }
-
-    return <Mfa />;
-  }
-  return (
-    <div className="mfa-box">
-      <BallTriangle fill="#000" />
-    </div>
+  const queryResult = useQuery<MfaRecord>('mfa-record', () =>
+    getMfaRecord(queryParameters.sessionToken),
   );
+
+  if (queryResult.status === 'loading') {
+    return (
+      <div className="mfa-box">
+        <BallTriangle fill="#000" />
+      </div>
+    );
+  }
+
+  if (queryResult.status === 'success') {
+    const mfaRecord = queryResult.data;
+
+    if (mfaRecord) {
+      if (mfaRecord.totpSecret) {
+        return <MfaSetup secret={mfaRecord.totpSecret} />;
+      }
+
+      return <Mfa />;
+    }
+  }
+
+  throw new Error('Invalid response');
 };
