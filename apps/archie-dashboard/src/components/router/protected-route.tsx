1c89fff3458df19600f7c7083a739cfd836b800d
Luka Furlan <luka@Lukas-MacBook-Pro.local>
2022-03-21 12:55:31 +0100

    Added login route with button that takes you to auth0

--- apps/archie-dashboard/src/components/router/protected-route.tsx
+++ apps/archie-dashboard/src/components/router/protected-route.tsx
@@ -1,19 +1,22 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { PropsWithChildren } from 'react';
+import { Navigate } from 'react-router-dom';
 
 export const ProtectedRoute: React.FC<PropsWithChildren<{}>> = ({
   children,
 }) => {
-  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div className='center-box'><h2>Loading</h2></div>
+    return (
+      <div className="center-box">
+        <h2>Loading</h2>
+      </div>
+    );
   }
 
   if (!isAuthenticated) {
-    loginWithRedirect();
-
-    return <></>
+    return <Navigate to="/login" />;
   }
 
   return <>{children}</>;
--- apps/archie-dashboard/src/routes/login/login-route.tsx
+++ apps/archie-dashboard/src/routes/login/login-route.tsx
@@ -1,9 +1,16 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from '@auth0/auth0-react';
 
 export const LoginRoute: React.FC = () => {
   const { loginWithRedirect } = useAuth0();
 
   // loginWithRedirect();
 
-  return <></>;
+  return (
+    <div className="center-box">
+      <h2>Signup</h2>
+      <button className={'button'} onClick={() => loginWithRedirect()}>
+        Log in
+      </button>
+    </div>
+  );
 };
