39813498092c02371b1347db718c5f0b589da634
jovanovskajovana <jovanovska.jovana1@gmail.com>
2022-05-17 11:41:40 +0200

    Try fixing fonts

--- apps/archie-dashboard/src/components/_generic/global_styles/font_face.styled.ts
+++ apps/archie-dashboard/src/components/_generic/global_styles/font_face.styled.ts
@@ -1,75 +1,75 @@
 import { createGlobalStyle } from 'styled-components'
 
-// import fontBlack from '../../../assets/fonts/Inter-Black.ttf'
-// import fontExtraBold from '../../../assets/fonts/Inter-ExtraBold.ttf'
-// import fontBold from '../../../assets/fonts/Inter-Bold.ttf'
-// import fontSemiBold from '../../../assets/fonts/Inter-SemiBold.ttf'
-// import fontMedium from '../../../assets/fonts/Inter-Medium.ttf'
-// import fontRegular from '../../../assets/fonts/Inter-Regular.ttf'
-// import fontLight from '../../..//assets/fonts/Inter-Light.ttf'
-// import fontExtraLight from '../../../assets/fonts/Inter-ExtraLight.ttf'
-// import fontThin from '../../../assets/fonts/Inter-Thin.ttf'
+import fontBlack from '../../../assets/fonts/Inter-Black.ttf'
+import fontExtraBold from '../../../assets/fonts/Inter-ExtraBold.ttf'
+import fontBold from '../../../assets/fonts/Inter-Bold.ttf'
+import fontSemiBold from '../../../assets/fonts/Inter-SemiBold.ttf'
+import fontMedium from '../../../assets/fonts/Inter-Medium.ttf'
+import fontRegular from '../../../assets/fonts/Inter-Regular.ttf'
+import fontLight from '../../..//assets/fonts/Inter-Light.ttf'
+import fontExtraLight from '../../../assets/fonts/Inter-ExtraLight.ttf'
+import fontThin from '../../../assets/fonts/Inter-Thin.ttf'
 
 const FontFace = createGlobalStyle`
   @font-face {
     font-family: 'Inter';
-    /* src: url($ {fontBlack}); */
+    src: url(${fontBlack});
     font-style: normal;
     font-weight: 900;
   }
 
   @font-face {
     font-family: 'Inter';
-    /* src: url($ {fontExtraBold}); */
+    src: url(${fontExtraBold});
     font-style: normal;
     font-weight: 800;
   }
 
   @font-face {
     font-family: 'Inter';
-    /* src: url($ {fontBold}); */
+    src: url(${fontBold});
     font-style: normal;
     font-weight: 700;
   }
 
   @font-face {
     font-family: 'Inter';
-    /* src: url($ {fontSemiBold}); */
+    src: url(${fontSemiBold});
     font-style: normal;
     font-weight: 600;
   }
 
   @font-face {
     font-family: 'Inter';
-    /* src: url($ {fontMedium}); */
+    src: url(${fontMedium});
     font-style: normal;
     font-weight: 500;
   }
 
   @font-face {
     font-family: 'Inter';
-    /* src: url($ {fontRegular}); */
+    src: url(${fontRegular});
     font-style: normal;
     font-weight: 400;
   }
 
   @font-face {
     font-family: 'Inter';
-    /* src: url($ {fontLight}); */
+    src: url(${fontLight});
     font-style: normal;
     font-weight: 300;
   }
 
   @font-face {
     font-family: 'Inter';
-    /* src: url($ {fontExtraLight}); */
+    src: url(${fontExtraLight});
     font-style: normal;
     font-weight: 200;
   }
 
   @font-face {
     font-family: 'Inter';
-    /* src: url($ {fontThin}); */
+    src: url(${fontThin});
     font-style: normal;
     font-weight: 100;
   }
