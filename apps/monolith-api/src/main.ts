abec773c397d49ee3515311f16f4e77437eda787
Luka <luka@archie.finance>
2022-05-29 19:52:29 +0200

    Added proxy config

--- apps/monolith-api/src/main.ts
+++ apps/monolith-api/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 import './tracer';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -20,6 +21,15 @@ async function bootstrap() {
       ],
     }),
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Archie')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
