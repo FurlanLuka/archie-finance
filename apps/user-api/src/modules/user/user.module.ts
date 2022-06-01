5d9c10d343d0ba2352201fcff27853abead7de06
Luka <luka@archie.finance>
2022-06-01 23:30:23 +0200

    Added internal user controller

--- apps/user-api/src/modules/user/user.module.ts
+++ apps/user-api/src/modules/user/user.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { Auth0Module } from '../auth0/auth0.module';
-import { UserController } from './user.controller';
+import { InternalUserController, UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [Auth0Module],
   providers: [UserService],
-  controllers: [UserController],
+  controllers: [UserController, InternalUserController],
 })
 export class UserModule {}
