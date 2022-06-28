9658468f9479bcaa78f420f328b02407c634daaa
Kerry Mahne <kerry.mahne@gmail.com>
2022-06-28 11:03:24 +0200

    fix interface export by adding a .babelrc file

--- libs/ui-theme/src/index.ts
+++ libs/ui-theme/src/index.ts
@@ -1,4 +1,5 @@
-export { theme, Theme } from './lib/theme';
+export { theme } from './lib/theme';
+export type { Theme } from './lib/theme';
 export * from './lib/elements';
 export { breakpoints } from './lib/breakpoints';
 export { GlobalStyles } from './lib/global-styles';
