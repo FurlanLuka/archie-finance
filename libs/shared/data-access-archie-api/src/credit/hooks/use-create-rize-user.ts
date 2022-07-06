e48319116eb29440cd6db6310cc66a2b2f37f36f
Andraz Cuderman <cuderman.andraz@gmail.com>
2022-07-06 08:37:31 +0200

    Update import

--- libs/shared/data-access-archie-api/src/credit/hooks/use-create-rize-user.ts
+++ libs/shared/data-access-archie-api/src/credit/hooks/use-create-rize-user.ts
@@ -4,7 +4,7 @@ import { useExtendedMutation } from '../../helper-hooks';
 import { DefaultVariables } from '../../helpers';
 import { ONBOARDING_RECORD_QUERY_KEY } from '../../onboarding/hooks/use-get-onboarding';
 import { MutationQueryResponse } from '../../interface';
-import { createRizeUser } from '../api/create-apto-user';
+import { createRizeUser } from '../api/create-rize-user';
 
 export const useCreateRizeUser = (): MutationQueryResponse => {
   const queryClient = useQueryClient();
