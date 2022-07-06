70453a6a8bf3be90132ef0eb81f637c1a119c405
Luka <luka@archie.finance>
2022-07-06 11:50:55 +0200

    Added connection to credit service

--- apps/credit-api/src/modules/apto/apto.module.ts
+++ apps/credit-api/src/modules/apto/apto.module.ts
@@ -8,6 +8,11 @@ import { AptoCard } from './apto_card.entity';
 import { AptoCardApplication } from './apto_card_application.entity';
 import { AptoUser } from './apto_user.entity';
 import { AptoVerification } from './apto_verification.entity';
+import {
+  SERVICE_QUEUE_NAME as ONBOARDING_SERVICE_QUEUE_NAME,
+  SERVICE_NAME as ONBOARDING_SERVICE_NAME,
+} from '@archie/api/onboarding-api/constants';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   controllers: [AptoController],
@@ -21,6 +26,16 @@ import { AptoVerification } from './apto_verification.entity';
     ]),
     AptoApiModule,
     CreditModule,
+    ClientsModule.register([
+      {
+        name: ONBOARDING_SERVICE_NAME,
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.QUEUE_URL],
+          queue: ONBOARDING_SERVICE_QUEUE_NAME,
+        },
+      },
+    ]),
   ],
 })
 export class AptoModule {}
