a54698fbb3c1f458b9a538e670868c985b823ae7
Andraz Cuderman <cuderman.andraz@gmail.com>
2022-07-06 12:59:19 +0200

    Added error logging to credit API apis

--- libs/tracing/src/lib/tracing.module.ts
+++ libs/tracing/src/lib/tracing.module.ts
@@ -1,16 +1,28 @@
-
-import { Catch, ArgumentsHost } from '@nestjs/common';
+import { Catch, ArgumentsHost, Logger } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import tracer, { Span } from 'dd-trace';
+
+interface RequestWithUser extends Request {
+  user?: {
+    sub: string;
+  };
+}
 
 @Catch()
 export class AllExceptionsFilter extends BaseExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
-    const span: Span = tracer.scope().active()
+    const ctx = host.switchToHttp();
+    const request: Request = ctx.getRequest<Request>();
+    const span: Span | null = tracer.scope().active();
 
     if (span !== null) {
-      console.log(exception)
-      span.setTag('error', exception)
+      span.setTag('error', exception);
+      span.setTag('userId', (<RequestWithUser>request).user?.sub);
+    } else {
+      Logger.error({
+        error: exception,
+        userId: (<RequestWithUser>request).user?.sub,
+      });
     }
 
     super.catch(exception, host);
