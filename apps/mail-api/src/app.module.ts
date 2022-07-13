19441fbc20d0c4e6a7310e5e35b1cd0cd2d90baf
Kerry Mahne <kerry.mahne@gmail.com>
2022-07-13 14:23:34 +0200

    fix mail api module config

--- apps/mail-api/src/app.module.ts
+++ apps/mail-api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@archie-microservices/config';
+import { ConfigModule, ConfigService } from '@archie-microservices/config';
 import { ConfigVariables } from '@archie/api/mail-api/constants';
 import { SendgridModule } from '@archie/api/mail-api/sendgrid';
 import { HealthModule } from '@archie-microservices/health';
+import { InternalApiModule } from '@archie-microservices/internal-api';
 
 @Module({
   imports: [
@@ -15,6 +16,13 @@ import { HealthModule } from '@archie-microservices/health';
         ConfigVariables.SENDGRID_VERIFY_EMAIL_TEMPLATAE_ID,
         ConfigVariables.ARCHIE_MARKETING_WEBSITE_URL,
       ],
+    }),
+    InternalApiModule.register({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        internalApiUrl: configService.get(ConfigVariables.INTERNAL_API_URL),
+      }),
     }),
     HealthModule,
     SendgridModule,
