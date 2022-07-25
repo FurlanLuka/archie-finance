019f79f670e7002206fc62089c9b1d1315a84ea3
Kerry Mahne <kerry.mahne@gmail.com>
2022-07-25 15:15:11 +0200

    narrow down the scope of query invalidation

--- libs/shared/data-access/archie-api/src/collateral/hooks/use-create-withdrawal.ts
+++ libs/shared/data-access/archie-api/src/collateral/hooks/use-create-withdrawal.ts
@@ -4,7 +4,6 @@ import { useExtendedMutation } from '../../helper-hooks';
 import { MutationQueryResponse } from '../../interface';
 import { createWithdrawal, CreateWithdrawalBody, WithdrawalResponse } from '../api/create-withdrawal';
 
-import { COLLATERAL_RECORD_QUERY_KEY } from './use-get-collateral';
 import { COLLATERAL_VALUE_RECORD_QUERY_KEY } from './use-poll-collateral-value';
 
 export const useCreateWithdrawal = (): MutationQueryResponse<CreateWithdrawalBody, WithdrawalResponse> => {
@@ -12,7 +11,7 @@ export const useCreateWithdrawal = (): MutationQueryResponse<CreateWithdrawalBod
 
   return useExtendedMutation<WithdrawalResponse, CreateWithdrawalBody>('collateral_withdraw', createWithdrawal, {
     onSuccess: async () => {
-      await queryClient.invalidateQueries([COLLATERAL_VALUE_RECORD_QUERY_KEY, COLLATERAL_RECORD_QUERY_KEY]);
+      await queryClient.invalidateQueries([COLLATERAL_VALUE_RECORD_QUERY_KEY]);
     },
   });
 };
