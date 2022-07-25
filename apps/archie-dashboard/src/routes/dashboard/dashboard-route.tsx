4beaf93452759cc4508a8d145f440504baa72da5
Kerry Mahne <kerry.mahne@gmail.com>
2022-07-25 13:04:55 +0200

    move ui/components to just components, move navigation there

--- apps/archie-dashboard/src/routes/dashboard/dashboard-route.tsx
+++ apps/archie-dashboard/src/routes/dashboard/dashboard-route.tsx
@@ -1,18 +1,17 @@
 import { FC } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
+import { Header, Navigation } from '@archie-webapps/archie-dashboard/components';
 import { HistoryScreen } from '@archie-webapps/archie-dashboard/feature/history';
 import { WalletAndCollateralScreen } from '@archie-webapps/archie-dashboard/feature/home';
 import { RewardsScreen } from '@archie-webapps/archie-dashboard/feature/rewards';
 import { SettingsScreen } from '@archie-webapps/archie-dashboard/feature/settings';
-import { Header } from '@archie-webapps/archie-dashboard/ui/components';
 import { QueryResponse, RequestState } from '@archie-webapps/shared/data-access/archie-api/interface';
 import { GetOnboardingResponse } from '@archie-webapps/shared/data-access/archie-api/onboarding/api/get-onboarding';
 import { useGetOnboarding } from '@archie-webapps/shared/data-access/archie-api/onboarding/hooks/use-get-onboarding';
 import { Loading, Page, Container } from '@archie-webapps/shared/ui/design-system';
 
 import { Setup2faBanner } from '../../components/banners/setup-2fa/setup-2fa';
-import { Navigation } from '../../components/navigation/navigation';
 
 export const DashboardRoute: FC = () => {
   const queryResponse: QueryResponse<GetOnboardingResponse> = useGetOnboarding();
--- libs/archie-dashboard/ui/components/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'archie-dashboard-ui-components',
-  preset: '../../../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/archie-dashboard/ui/components',
-};
