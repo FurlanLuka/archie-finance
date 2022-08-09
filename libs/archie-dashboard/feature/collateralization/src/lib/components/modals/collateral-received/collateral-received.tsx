a524c90c15676bbd62c77c49838825897eecc8bd
jovanovskajovana <jovanovska.jovana1@gmail.com>
2022-08-09 17:26:12 +0200

    Update layout for collateral received modal in dashboard

--- libs/archie-dashboard/feature/collateralization/src/lib/components/modals/collateral-received/collateral-received.tsx
+++ libs/archie-dashboard/feature/collateralization/src/lib/components/modals/collateral-received/collateral-received.tsx
@@ -3,6 +3,8 @@ import { useTranslation } from 'react-i18next';
 
 import { ButtonPrimary, Modal, ParagraphM, ParagraphXS } from '@archie-webapps/shared/ui/design-system';
 
+import imgCollateralReceived from '../../../../assets/img-collateral-received.png';
+
 import { CollateralReceivedModalStyled } from './collateral-received.styled';
 
 interface CollateralReceivedModalProps {
@@ -21,16 +23,21 @@ export const CollateralReceivedModal: FC<CollateralReceivedModalProps> = ({
   return (
     <Modal isOpen={true} maxWidth="800px">
       <CollateralReceivedModalStyled>
-        <ParagraphM weight={700}>{t('dashboard_collateralization.collateral_received_modal.title')}</ParagraphM>
-        <ParagraphXS>
-          {t('dashboard_collateralization.collateral_received_modal.text', {
-            collateral: collateralText,
-            credit_value: creditValue.toFixed(2),
-          })}
-        </ParagraphXS>
-        <ButtonPrimary onClick={onConfirm} maxWidth="fit-content">
-          {t('dashboard_collateralization.collateral_received_modal.btn')}
-        </ButtonPrimary>
+        <div className="image">
+          <img src={imgCollateralReceived} alt={t('collateral_received_modal.img_alt')} />
+        </div>
+        <div className="content">
+          <ParagraphM weight={700}>{t('dashboard_collateralization.collateral_received_modal.title')}</ParagraphM>
+          <ParagraphXS>
+            {t('dashboard_collateralization.collateral_received_modal.text', {
+              collateral: collateralText,
+              credit_value: creditValue.toFixed(2),
+            })}
+          </ParagraphXS>
+          <ButtonPrimary onClick={onConfirm} maxWidth="fit-content">
+            {t('dashboard_collateralization.collateral_received_modal.btn')}
+          </ButtonPrimary>
+        </div>
       </CollateralReceivedModalStyled>
     </Modal>
   );
