59535fb59e6ffef603e7dabada74f77319d2504b
Andraz Cuderman <cuderman.andraz@gmail.com>
2022-08-18 15:18:58 +0200

    Rebuild openapi

--- libs/api/peach-api/borrower/src/lib/borrower.service.ts
+++ libs/api/peach-api/borrower/src/lib/borrower.service.ts
@@ -31,10 +31,7 @@ import {
 import { WebhookPaymentPayload } from '@archie/api/webhook-api/data-transfer-objects';
 import { CreditLinePaymentReceivedPayload } from '@archie/api/peach-api/data-transfer-objects';
 import { ObligationsResponseDto, ScheduleTransactionDto } from './borrower.dto';
-import {
-  BorrowerNotFoundError,
-  PlaidPaymentInstrumentNotFoundError,
-} from './borrower.errors';
+import { BorrowerNotFoundError } from './borrower.errors';
 
 @Injectable()
 export class PeachBorrowerService {
