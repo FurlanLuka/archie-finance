917102b2cfbf263dcac5aee6a8a512d397be25bb
jovanovskajovana <jovanovska.jovana1@gmail.com>
2022-08-19 13:55:46 +0200

    Update payment account select dropdown

--- libs/archie-dashboard/feature/payments/src/lib/components/account-item/account-item.styled.ts
+++ libs/archie-dashboard/feature/payments/src/lib/components/account-item/account-item.styled.ts
@@ -6,6 +6,14 @@ export const AccountItemStyled = styled.div`
   align-items: center;
   justify-content: flex-start;
 
+  .circle {
+    background-color: ${({ theme }) => theme.textDisabled};
+    width: 1.5rem;
+    height: 1.5rem;
+    border-radius: 100%;
+    margin-right: 0.5rem;
+  }
+
   .account-info {
     display: flex;
     flex-direction: column;
@@ -23,11 +31,7 @@ export const AccountItemStyled = styled.div`
     color: ${({ theme }) => theme.textSecondary};
   }
 
-  .circle {
-    background-color: ${({ theme }) => theme.textDisabled};
-    width: 1.5rem;
-    height: 1.5rem;
-    border-radius: 50%;
-    margin-right: 0.5rem;
+  p {
+    line-height: 1.2;
   }
 `;
