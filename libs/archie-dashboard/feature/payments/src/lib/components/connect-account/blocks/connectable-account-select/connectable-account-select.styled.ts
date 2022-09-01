a7e2b1f0f4d57cdb77e91fdd7fd35d841d416ba6
jovanovskajovana <jovanovska.jovana1@gmail.com>
2022-09-01 15:27:38 +0200

    Fix account selecting modal

--- libs/archie-dashboard/feature/payments/src/lib/components/connect-account/blocks/connectable-account-select/connectable-account-select.styled.ts
+++ libs/archie-dashboard/feature/payments/src/lib/components/connect-account/blocks/connectable-account-select/connectable-account-select.styled.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const ConnectableAccountSelectStyled = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
   min-height: 176px;
 
   .loader {
@@ -14,6 +15,7 @@ export const ConnectableAccountSelectStyled = styled.div`
   }
 
   .modal-select {
+    width: 100%;
     margin-bottom: 2rem;
   }
 `;
