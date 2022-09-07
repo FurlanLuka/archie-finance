9fb17354ebef811a955089b536eee3377b5b6bc2
Andraz Cuderman <cuderman.andraz@gmail.com>
2022-09-07 12:17:26 +0200

    Fixed margin entity imports

--- libs/api/margin-api/margin/src/lib/utils/margin_action_handlers/margin_action_handlers.module.ts
+++ libs/api/margin-api/margin/src/lib/utils/margin_action_handlers/margin_action_handlers.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MarginCallInDangerHandlerService } from './margin_call_in_danger_handler.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MarginNotification } from '@archie/api/credit-api/margin';
 import { MarginCallHandlerService } from './margin_call_handler.service';
 import { LiquidationUtilService } from './liquidation.service';
 import { MarginCallPriceFactory } from './margin_call_price.factory';
 import { MarginCall } from '../../margin_calls.entity';
 import { MarginCheck } from '../../margin_check.entity';
+import { MarginNotification } from '../../margin_notifications.entity';
 
 @Module({
   controllers: [],
