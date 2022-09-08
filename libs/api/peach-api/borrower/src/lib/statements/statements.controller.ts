f9af2bece908a2fef06c7242e107c8e2a1813c5e
Kerry Mahne <kerry.mahne@gmail.com>
2022-09-08 15:11:58 +0200

    lint

--- libs/api/peach-api/borrower/src/lib/statements/statements.controller.ts
+++ libs/api/peach-api/borrower/src/lib/statements/statements.controller.ts
@@ -1,14 +1,7 @@
 import { AuthGuard } from '@archie/api/utils/auth0';
 import { ApiErrorResponse } from '@archie/api/utils/openapi';
 import { Statement } from '../api/peach_api.interfaces';
-import {
-  Controller,
-  Get,
-  NotFoundException,
-  Param,
-  Req,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import {
   BorrowerNotFoundError,
