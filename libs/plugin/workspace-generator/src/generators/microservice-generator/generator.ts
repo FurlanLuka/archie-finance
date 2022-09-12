d6861a4e44b0983f500ca26f23597d3d199bb6b5
Luka <luka@archie.finance>
2022-09-12 12:41:20 +0200

    Made microservice generator generate lib

--- libs/plugin/workspace-generator/src/generators/microservice-generator/generator.ts
+++ libs/plugin/workspace-generator/src/generators/microservice-generator/generator.ts
@@ -2,7 +2,7 @@ import { getWorkspaceLayout, joinPathFragments, Tree } from '@nrwl/devkit';
 import { MicroserviceGenerator } from './schema';
 import { initGenerator } from '@nrwl/nest';
 import { applicationGenerator as nodeApplicationGenerator } from '@nrwl/node';
-import { createAppFiles } from './lib/create-files';
+import { createAppFiles, createLibFiles } from './lib/create-files';
 import { updateTsConfig } from './lib/update-tsconfig';
 import { runTasksInSerial } from '@nrwl/workspace/src/utilities/run-tasks-in-serial';
 import { microserviceModuleGenerator } from '../microservice-module-generator/generator';
@@ -18,12 +18,12 @@ function normalizeOptions(
 ): NormalizedSchema {
   const projectRoot = joinPathFragments(
     getWorkspaceLayout(tree).appsDir,
-    options.name
+    options.name,
   );
 
   const constantsRoot = joinPathFragments(
     getWorkspaceLayout(tree).libsDir,
-    `api/${options.name}/constants`
+    `api/${options.name}/constants`,
   );
 
   return {
@@ -39,12 +39,12 @@ export default async function (tree: Tree, options: MicroserviceGenerator) {
   const initTask = await initGenerator(tree, {
     unitTestRunner: 'jest',
     skipFormat: true,
-  })
+  });
 
   const nodeApplicationTask = await nodeApplicationGenerator(tree, {
     unitTestRunner: 'jest',
     name: normalizedOptions.name,
-  })
+  });
 
   createAppFiles(tree, normalizedOptions);
   updateTsConfig(tree, normalizedOptions);
@@ -52,7 +52,13 @@ export default async function (tree: Tree, options: MicroserviceGenerator) {
   const createConstantsLibraryTask = await microserviceModuleGenerator(tree, {
     projectName: options.name,
     name: 'constants',
-  })
+  });
 
-  return runTasksInSerial(initTask, nodeApplicationTask, createConstantsLibraryTask);
+  createLibFiles(tree, normalizedOptions);
+
+  return runTasksInSerial(
+    initTask,
+    nodeApplicationTask,
+    createConstantsLibraryTask,
+  );
 }
