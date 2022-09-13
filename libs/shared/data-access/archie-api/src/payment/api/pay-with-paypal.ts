a8cff954313e03a3afa6906c958edbe62ad34f9c
jovanovskajovana <jovanovska.jovana1@gmail.com>
2022-09-13 02:15:00 +0200

    Prepare response from mutation

--- libs/shared/data-access/archie-api/src/payment/api/pay-with-paypal.ts
+++ libs/shared/data-access/archie-api/src/payment/api/pay-with-paypal.ts
@@ -1,18 +1,16 @@
 import { API_URL } from '../../constants';
 import { DefaultVariables, postRequest } from '../../helpers';
 
-export interface PayWithPaypalParams {
+export interface PayWithPaypalPayload extends DefaultVariables {
   paymentAmount: number;
 }
 
-export interface PayWithPaypalParamsBody extends DefaultVariables, PayWithPaypalParams {}
-
 export const ERROR_LIST = new Map<string, string>([]);
 
-export const payWithPaypal = async ({ accessToken, ...body }: PayWithPaypalParamsBody): Promise<void> => {
+export const payWithPaypal = async ({ accessToken, ...payload }: PayWithPaypalPayload): Promise<void> => {
   return postRequest(
     `${API_URL}/v1/paypal/orders`,
-    body,
+    payload,
     {
       headers: {
         Authorization: `Bearer ${accessToken}`,
--- libs/shared/data-access/archie-api/src/payment/hooks/use-pay-with-paypal.ts
+++ libs/shared/data-access/archie-api/src/payment/hooks/use-pay-with-paypal.ts
@@ -1,7 +1,16 @@
+import { useQueryClient } from 'react-query';
+
 import { useExtendedMutation } from '../../helper-hooks';
 import { MutationQueryResponse } from '../../interface';
-import { PayWithPaypalParamsBody, payWithPaypal } from '../api/pay-with-paypal';
+import { payWithPaypal, PayWithPaypalPayload } from '../api/pay-with-paypal';
 
-export const usePayWithPaypal = (): MutationQueryResponse<PayWithPaypalParamsBody, void> => {
-  return useExtendedMutation<void, PayWithPaypalParamsBody>('paypal_record', payWithPaypal);
+export const usePayWithPaypal = (): MutationQueryResponse<PayWithPaypalPayload, void> => {
+  const queryClient = useQueryClient();
+
+  return useExtendedMutation<void, PayWithPaypalPayload>('paypal_record', payWithPaypal, {
+    onSuccess: data => {
+      console.log(data)
+      queryClient.setQueryData('paypal_response', data);
+    }
+  });
 };
