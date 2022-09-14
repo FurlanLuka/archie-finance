c79cfce66ce26ee43da2af1ca33953bd9da99e04
Andraz Cuderman <cuderman.andraz@gmail.com>
2022-09-14 17:13:03 +0200

    Added last adjustment migration

--- apps/credit-api/src/migrations.ts
+++ apps/credit-api/src/migrations.ts
@@ -1,3 +1,7 @@
 import { CreditApi1658825191535 } from './migrations/CreditApi1658825191535';
+import { CreditApi1663168364110 } from './migrations/CreditApi1663168364110';
 
-export const migrations = [CreditApi1658825191535];
+export const migrations = [
+  CreditApi1658825191535,
+  CreditApi1663168364110,
+  ]
