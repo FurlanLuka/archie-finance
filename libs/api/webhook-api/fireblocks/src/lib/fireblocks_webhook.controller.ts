1e900e9527ccc66b5ceb66babecc695e383b1bdd
Luka <luka@archie.finance>
2022-10-03 10:38:48 +0200

    Added logging to webhook

--- libs/api/webhook-api/fireblocks/src/lib/fireblocks_webhook.controller.ts
+++ libs/api/webhook-api/fireblocks/src/lib/fireblocks_webhook.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Logger, Post, UseGuards } from '@nestjs/common';
 import { FireblocksWebhookService } from './fireblocks_webhook.service';
 import { FireblocksWebhookGuard } from './guard/fireblocks_webhook.guard';
 
@@ -9,6 +9,7 @@ export class FireblocksWebhookController {
   @Post('')
   @UseGuards(FireblocksWebhookGuard)
   public async webhook(@Body() body) {
+    Logger.log(JSON.stringify(body));
     return this.fireblocksWebhookService.webhookHandler(body);
   }
 }
