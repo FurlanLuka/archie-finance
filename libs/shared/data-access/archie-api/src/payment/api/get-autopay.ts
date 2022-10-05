cd2db0929090e29cca9d0405f90737baf285258b
Kerry Mahne <kerry.mahne@gmail.com>
2022-10-05 15:33:16 +0200

    custom error attributes so we can discern what error was received

--- libs/shared/data-access/archie-api/src/payment/api/get-autopay.ts
+++ libs/shared/data-access/archie-api/src/payment/api/get-autopay.ts
@@ -2,7 +2,8 @@ import { API_URL } from '../../constants';
 import { getRequest } from '../../helpers';
 import { Autopay } from '../payment.interfaces';
 
-export const ERROR_LIST = new Map<string, string>([['AUTOPAY_NOT_CONFIGURED', 'Autopay not configured']]);
+export const AUTOPAY_NOT_CONFIGURED = 'AUTOPAY_NOT_CONFIGURED';
+export const ERROR_LIST = new Map<string, string>([[AUTOPAY_NOT_CONFIGURED, 'Autopay not configured']]);
 
 export const getAutopay = async (accessToken: string): Promise<Autopay> => {
   return getRequest<Autopay>(
--- libs/shared/data-access/archie-api/src/payment/hooks/use-get-autopay.ts
+++ libs/shared/data-access/archie-api/src/payment/hooks/use-get-autopay.ts
@@ -1,10 +1,24 @@
+import { ApiError } from '../../api-error';
 import { useExtendedQuery } from '../../helper-hooks';
 import { QueryResponse } from '../../interface';
-import { getAutopay } from '../api/get-autopay';
+import { AUTOPAY_NOT_CONFIGURED, getAutopay } from '../api/get-autopay';
 import { Autopay } from '../payment.interfaces';
 
 const AUTOPAY_RECORD_QUERY_KEY = 'autopay_record';
 
-export const useGetAutopay = (): QueryResponse<Autopay> => {
-  return useExtendedQuery(AUTOPAY_RECORD_QUERY_KEY, async (accessToken: string) => getAutopay(accessToken));
+export const useGetAutopay = (): QueryResponse<Autopay | null> => {
+  return useExtendedQuery(AUTOPAY_RECORD_QUERY_KEY, async (accessToken: string) => {
+    try {
+      const response = await getAutopay(accessToken);
+      return response;
+    } catch (err) {
+      if (err instanceof ApiError) {
+        if (err.statusCode === 404 && err.message === AUTOPAY_NOT_CONFIGURED) {
+          return null;
+        }
+      }
+
+      throw err;
+    }
+  });
 };
