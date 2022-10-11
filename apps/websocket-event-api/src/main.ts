477fb17956f870fea3a29ddce71b5c1ffe332ca7
Andraz Cuderman <cuderman.andraz@gmail.com>
2022-10-11 14:51:37 +0200

    Fixed service service & ws enable flow

--- apps/websocket-event-api/src/main.ts
+++ apps/websocket-event-api/src/main.ts
@@ -3,11 +3,11 @@ import './tracer';
 import { start } from '@archie/api/utils/microservice';
 import { SERVICE_NAME } from '@archie/api/websocket-event-api/constants';
 import { Logger } from '@nestjs/common';
-import { WsAdapter } from '@archie/api/websocket-event-api/websocket-event';
 
 async function bootstrap(): Promise<void> {
-  const app = await start(SERVICE_NAME, AppModule);
-  app?.useWebSocketAdapter(new WsAdapter(app));
+  await start(SERVICE_NAME, AppModule, {
+    enableWs: true,
+  });
 }
 
 bootstrap().catch((error) => {
--- libs/api/utils/microservice/src/index.ts
+++ libs/api/utils/microservice/src/index.ts
@@ -1,10 +1,11 @@
 import { generateMigration } from './lib/generate-migration';
-import { startService } from './lib/start-service';
+import { startService, StartServiceOptions } from './lib/start-service';
 import { INestApplication } from '@nestjs/common';
 
 export async function start(
   appName: string,
   module: unknown,
+  options?: StartServiceOptions,
 ): Promise<INestApplication | void> {
   const generateMigrations: boolean = process.argv.some(
     (arg) => arg === 'generate-migration',
@@ -13,6 +14,6 @@ export async function start(
   if (generateMigrations) {
     await generateMigration(appName, module);
   } else {
-    await startService(appName, module);
+    await startService(appName, module, options);
   }
 }
