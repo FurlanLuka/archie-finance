f3dc60d649c497938eb35a2310188b3aa7896ce6
Andraz Cuderman <cuderman.andraz@gmail.com>
2022-10-18 10:12:28 +0200

    Change auth0 route to plural

--- libs/api/webhook-api/auth0/src/lib/auth0.controller.ts
+++ libs/api/webhook-api/auth0/src/lib/auth0.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { Auth0Logs } from './auth0.interfaces';
 import { Auth0Service } from './auth0.service';
 
-@Controller('v1/webhook/auth0')
+@Controller('v1/webhooks/auth0')
 export class Auth0Controller {
   constructor(private auth0Service: Auth0Service) {}
 
