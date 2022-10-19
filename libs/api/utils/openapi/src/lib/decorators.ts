6f28d1d7b2528bbaa1927a2364c106a6d85d9049
Andraz Cuderman <cuderman.andraz@gmail.com>
2022-10-19 15:55:56 +0200

    Added logic to show all possible status codes even when there are multiple for the same

--- libs/api/utils/openapi/src/lib/decorators.ts
+++ libs/api/utils/openapi/src/lib/decorators.ts
@@ -2,6 +2,8 @@ import { ApiResponse } from '@nestjs/swagger';
 import { HttpException } from '@nestjs/common';
 import { ClassConstructor } from 'class-transformer';
 
+type ErrorsByStatus = Record<string, HttpException[]>;
+
 export function ApiErrorResponse<T extends ClassConstructor<HttpException>>(
   errors: T[],
 ) {
@@ -10,12 +12,27 @@ export function ApiErrorResponse<T extends ClassConstructor<HttpException>>(
     propertyKey: string,
     descriptor: PropertyDescriptor,
   ): void => {
-    errors.forEach((Error: T) => {
-      const initializedError: HttpException = new Error();
+    const errorsByStatus = errors.reduce(
+      (errorsGroupedByStatusCode: ErrorsByStatus, Error: T) => {
+        const initializedError: HttpException = new Error();
+        const errorStatus: string = initializedError.getStatus().toString();
 
+        errorsGroupedByStatusCode[errorStatus] =
+          errorsGroupedByStatusCode[errorStatus] === undefined
+            ? [initializedError]
+            : [...errorsGroupedByStatusCode[errorStatus], initializedError];
+
+        return errorsGroupedByStatusCode;
+      },
+      {},
+    );
+
+    Object.keys(errorsByStatus).forEach((status) => {
       ApiResponse({
-        status: initializedError.getStatus(),
-        description: initializedError.message,
+        status: Number(status),
+        description: errorsByStatus[status]
+          .map((error) => error.message)
+          .join(' | '),
       })(target, propertyKey, descriptor);
     });
   };
