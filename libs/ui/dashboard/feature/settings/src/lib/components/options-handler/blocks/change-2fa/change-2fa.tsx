9db3b9e1660c536f2cbe36b69b573535f2868090
jovanovskajovana <jovanovska.jovana1@gmail.com>
2022-10-27 15:49:53 +0200

    Use remove device instead of change

--- libs/ui/dashboard/feature/settings/src/lib/components/options-handler/blocks/change-2fa/change-2fa.tsx
+++ libs/ui/dashboard/feature/settings/src/lib/components/options-handler/blocks/change-2fa/change-2fa.tsx
@@ -5,7 +5,7 @@ import { RequestState } from '@archie/ui/shared/data-access/archie-api/interface
 import { useGetOnboarding } from '@archie/ui/shared/data-access/archie-api/onboarding/hooks/use-get-onboarding';
 import { useGetMfaEnrollments } from '@archie/ui/shared/data-access/archie-api/user/hooks/use-get-mfa-enrollments';
 import { useRemoveMfaEnrollment } from '@archie/ui/shared/data-access/archie-api/user/hooks/use-remove-mfa-enrollment';
-import { useStartMfaEnrollment } from '@archie/ui/shared/data-access/archie-api/user/hooks/use-start-mfa-enrollment';
+import { queryClient } from '@archie/ui/shared/data-access/query-client';
 
 import { Change2faConfirmationModal } from '../../../modals/change-2fa-confirmation/change-2fa-confirmation';
 import { OptionsItem } from '../../../options-item/options-item';
@@ -19,39 +19,26 @@ export const Change2FA: FC = () => {
   const getOnboardinResponse = useGetOnboarding();
   const getMfaEnrollmentsResponse = useGetMfaEnrollments();
   const removeMfaEnrollmentMutation = useRemoveMfaEnrollment(mfaEnrollmentId);
-  const startMfaEnrollmentMutation = useStartMfaEnrollment();
 
   const isMfaSet = getOnboardinResponse.state === RequestState.SUCCESS && getOnboardinResponse.data.mfaEnrollmentStage;
 
   useEffect(() => {
+    queryClient.invalidateQueries('mfa_enrollments_record');
+  }, [isMfaSet]);
+
+  useEffect(() => {
     if (isMfaSet) {
       if (getMfaEnrollmentsResponse.state === RequestState.SUCCESS) {
-        setMfaEndollmentId(getMfaEnrollmentsResponse.data[0].id);
+        if (getMfaEnrollmentsResponse.data.length > 0) {
+          setMfaEndollmentId(getMfaEnrollmentsResponse.data[0].id);
+        }
       }
     }
   }, [isMfaSet, getMfaEnrollmentsResponse]);
 
-  console.log(removeMfaEnrollmentMutation.state);
-
-  useEffect(() => {
-    if (removeMfaEnrollmentMutation.state === RequestState.SUCCESS) {
-      if (startMfaEnrollmentMutation.state === RequestState.IDLE) {
-        startMfaEnrollmentMutation.mutate({});
-      }
-
-      if (startMfaEnrollmentMutation.state === RequestState.SUCCESS) {
-        window.open(startMfaEnrollmentMutation.data.ticket_url, '_blank');
-      }
-    }
-  }, [removeMfaEnrollmentMutation.state, startMfaEnrollmentMutation.state]);
-
   const handleClick = () => {
     if (mfaEnrollmentId) {
-      if (
-        removeMfaEnrollmentMutation.state === RequestState.IDLE ||
-        // ni prav da se klice on SUCCESS state. kako lahko dosezemo da je spet nazaj IDLE
-        removeMfaEnrollmentMutation.state === RequestState.SUCCESS
-      ) {
+      if (removeMfaEnrollmentMutation.state === RequestState.IDLE) {
         removeMfaEnrollmentMutation.mutate({});
         setChange2faConfirmatinModalOpen(false);
       }
