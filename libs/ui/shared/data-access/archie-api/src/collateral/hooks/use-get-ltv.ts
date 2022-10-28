b65a0ab48bfdcb78792c61c39706304ef726551f
Kerry Mahne <kerry.mahne@gmail.com>
2022-10-28 10:30:24 +0200

    ltv fe rework

--- libs/ui/shared/data-access/archie-api/src/collateral/hooks/use-get-ltv.ts
+++ libs/ui/shared/data-access/archie-api/src/collateral/hooks/use-get-ltv.ts
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 
+import { Ltv } from '@archie/api/ltv-api/data-transfer-objects/types';
 import {
   websocketInstance,
   WsEventTopic,
@@ -7,13 +8,13 @@ import {
 
 import { useExtendedQuery } from '../../helper-hooks';
 import { QueryResponse } from '../../interface';
-import { getLTV, LTV } from '../api/get-ltv';
+import { getLTV } from '../api/get-ltv';
 import { handleLtvUpdatedEvent } from '../event-handlers/handle-ltv-updated';
 
 export const LTV_RECORD_QUERY_KEY = 'ltv_record';
 const LTV_UPDATED_HANDLER_ID = 'useGetLTV.ltv-updated';
 
-export const useGetLTV = (): QueryResponse<LTV> => {
+export const useGetLTV = (): QueryResponse<Ltv> => {
   useEffect(() => {
     websocketInstance.addHandler(
       WsEventTopic.LTV_UPDATED_TOPIC,
