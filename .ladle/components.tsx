3a28046b7955344f8ece919bc097e8bb18b07e35
jovanovskajovana <jovanovska.jovana1@gmail.com>
2022-11-10 18:38:34 +0100

    Move external font to head config

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -6,10 +6,6 @@ import { GlobalStyles, theme } from '@archie/ui/shared/theme';
 
 export const Provider: GlobalProvider = ({ children }) => (
   <>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-      rel="stylesheet"
-    />
     <GlobalStyles />
     <ThemeProvider theme={theme}>{children}</ThemeProvider>
   </>
